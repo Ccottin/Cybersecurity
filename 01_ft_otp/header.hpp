794a2f68172cb18a2f6471c57be65fa78f3089a9
Clara COTTIN <ccottin@made-f0Ar7s5.clusters.42paris.fr>
2023-10-14 15:30:12 +0200

    timestamps needed, as well as hash algo

--- 01_ft_otp/header.hpp
+++ 01_ft_otp/header.hpp
@@ -6,7 +6,7 @@
 /*   By: ccottin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 19:45:03 by ccottin           #+#    #+#             */
-/*   Updated: 2023/10/13 21:00:34 by ccottin          ###   ########.fr       */
+/*   Updated: 2023/10/14 15:29:38 by ccottin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 # include <cstring>
 # include <string>
 # include <sstream>
+# include <cctype>
 
 #endif
--- 01_ft_otp/main.cpp
+++ 01_ft_otp/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ccottin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 19:43:44 by ccottin           #+#    #+#             */
-/*   Updated: 2023/10/13 21:09:51 by ccottin          ###   ########.fr       */
+/*   Updated: 2023/10/14 14:32:42 by ccottin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,28 +45,57 @@ std::string	file_manager(const char *filename, std::ios_base::openmode mode,
 	return (ret);
 }
 
-int			ft_error(void)
+void		ft_error(int err)
 {
-	std::cout << "Usage : ./ft_otp [-g key file] or [-k password file]";
-	return (1);
+	switch (err) 
+	{
+		case 1:
+			std::cerr << "Usage : ./ft_otp [-g key file] or [-k password file]";
+			break ;
+		case 2:
+			std::cerr << "error: key must be 64 hexadecimal characters.";
+			break ;
+		case 3:
+			std::cerr << "error: key initialasation went wrong.";
+			break ;
+
+	}
+	exit(1);
+}
+
+int			time_steps_calculator(void)
+{
+	
+}
+
+void		generate_password(void)
+{
+	std::string	key = file_manager("ft_opt.key", std::ios_base::out, NULL);
+	if (key.empty())
+		ft_error(3);
+	//time steps needed
 }
 
 void		stock_new_key(std::string key)
 {
-	//open modes neededs = trunc for discarding all previous contents
-//in for input
+	int		i;
 
-
+	i = 0;
+	while (std::isalnum(key[i]))
+		++i;
+	if (i != key.length() || i < 64)
+		ft_error(2);
+	file_manager("ft_otp.key", std::ios_base::trunc | std::ios_base::in, key.c_str());
 }
 
 int			main(int ac, char **av)
 {
 	if (ac != 3 || strncmp(av[1], "-g", 3) || strncmp(av[1], "-k", 3)
 				|| (!strncmp(av[1], "-k", 3) && strncmp(av[2], "ft_otp.key", 11)))
-		return (ft_error());
+		ft_error(1);
 	
 	if (strncmp(av[1], "-g", 3))
 		stock_new_key(av[2]);
 	if (strncmp(av[1], "-k", 3))
-		//generate password
+		generate_password();
 }
