3439d034df32b4ac14584aa593751648e9e24098
Clara COTTIN <ccottin@made-f0Cr7s2.clusters.42paris.fr>
2023-10-18 21:13:43 +0200

    still on hmac

--- 01_ft_otp/main.cpp
+++ 01_ft_otp/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ccottin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 19:43:44 by ccottin           #+#    #+#             */
-/*   Updated: 2023/10/15 20:15:13 by ccottin          ###   ########.fr       */
+/*   Updated: 2023/10/18 21:13:23 by ccottin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,10 +69,10 @@ void		ft_error(int err)
 	exit(1);
 }
 
-long int		time_steps_calculator(void)
+int				time_steps_calculator(void)
 {
 	time_t	current_time;
-	long int		time_ref;
+	int		time_ref;
 	int		x;
 
 	if (TIME_STEPS == 0)
@@ -85,38 +85,72 @@ long int		time_steps_calculator(void)
 
 }
 
-std::string	call_SHA_1(const char *to_hash)
+void		num_to_string(int number, unsigned char *ret)
 {
-    unsigned char	hashed[SHA_DIGEST_LENGTH];
-
-	//this is chatgpts's so if something doenst work try a differnet cast
-    SHA1(reinterpret_cast<const unsigned char*>(to_hash), std::strlen(to_hash), hashed);
-	std::string ret(reinterpret_cast<char*>(hashed));
-	return (ret);
+	//recheck this in order to make the string fits perfectly w/ this one and the concat
+	for (unsigned int i = sizeof(int); i > 0; --i)
+	{
+		ret[i] = number;
+		printf("%d, %d\n", ret[i], i);
+		number >>= 8;
+	}
+	ret[sizeof(int)] = 0;
 }
 
-void		hmac_calculator(std::string key, long int time_ref)
+unsigned char	*call_SHA_1(const unsigned char *to_hash, unsigned char *hashed)
 {
-	unsigned char	ipad[65];				//create 2 string with fixed values to
-	unsigned char	opad[65];				//apply a XOR operation on the hashed output
-	unsigned char	key_c[65];				//later on.
-											//ipad = in padding outpad = outpadding
-	memset(ipad, 0x36, 64);					//this algo helps with eccficiency issues
-	memset(opad, 0x5c, 64);					//by precomputing hashed keys, making the auth
-	memset(key_c, 0, 64);					//check faster for tres demandes servers
-	ipad[64] = 0;
-	opad[64] = 0;
+    SHA1(to_hash, strlen((const char *)to_hash), hashed);
+	hashed[SHA_DIGEST_LENGTH] = 0;
+	return (hashed);
+}
 
-	std::cout << key << time_ref;	
+void		hmac_calculator(std::string key, int time_ref)
+{
+	unsigned char	ipad[65 + sizeof(int)];				//create 2 string with fixed values to
+	unsigned char	opad[65];							//apply a XOR operation on the hashed output
+	unsigned char	key_c[65];							//later on.
+	unsigned char	time_str[sizeof(int) + 1];			//ipad = inner padding opad = outer
+	unsigned char	hashed[SHA_DIGEST_LENGTH + 1];		//this algo helps with eccficiency issues
+													//by precomputing hashed keys, making the auth
+	memset(key_c, 0, 65);							//check faster for tres demandes servers
+	memset(key_c, 0, 65);							//check faster for tres demandes servers
+	memset(ipad, 0, 65 + sizeof(int));	
+	
+	std::cout << key << "=========" << time_ref << "=======" << std::endl;	
 	//first we need the key to be no longer than the block of data B for SHA = 64
 	if (key.length() > 64)
-		key = call_SHA_1(key.c_str());
-	std::cout << SHA_DIGEST_LENGTH;
+		memcpy(key_c, call_SHA_1((const unsigned char*)key.c_str(), hashed), 20);
 	//we should print the key in a bloc of memory B-sized to XOR it
-	memcpy(key_c, key.c_str(), key.length());	
+	else
+		memcpy(key_c, key.c_str(), key.length());	
 	//then we'll do SHA(Key XOR opad, (SHA(key XOR ipad, time))) step by step
-	//std::cout << key + time_ref;	
-		//find a way to add the value of time_ref at the end of the key
+
+	//	std::cout << key_c << "=========" << time_ref << "=======" << std::endl;
+
+	memcpy(ipad, key_c, 65);	
+	memcpy(opad, key_c, 65);	
+	for (int i = 0; i < 64; i++) {
+                ipad[i] ^= 0x36;
+                opad[i] ^= 0x5c;
+        }
+	for (unsigned int i = 0; i < strlen((char*)ipad); i++)
+		printf("%d = %d\n", i, ipad[i]);
+
+	num_to_string(time_ref, time_str);
+	for (unsigned int i = 0; i < strlen((char*)ipad); i++)
+		printf("%d = %d\n", i, time_str[i]);
+
+	//im tired and i cannot make this works
+	memcpy(&ipad[strlen((char*)ipad)], time_str, strlen((char*)time_str));
+
+	for (unsigned int i = 0; i < strlen((char*)ipad); i++)
+		printf("%d = %d\n", i, ipad[i]);
+	call_SHA_1((unsigned char*)ipad, hashed);
+
+	for (unsigned int i = 0; i < strlen((char*)hashed); i++)
+		printf("%d = %d\n", i, hashed[i]);
+
+	
 }
 
 void		hotp_calculator(std::string key, long int time_ref)
